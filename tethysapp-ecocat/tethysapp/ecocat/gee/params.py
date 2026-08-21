ea325daa33a900f04abf306e081c57ad94d7d325
Daniel Le Corre <D.LeCorre@kew.org>
2026-08-21 13:32:06 +0100

    Removing key

--- tethysapp-ecocat/tethysapp/ecocat/gee/params.py
+++ tethysapp-ecocat/tethysapp/ecocat/gee/params.py
@@ -1,2 +1,2 @@
-service_account = 'ecocat-dev@ecocat-dev.iam.gserviceaccount.com'
-private_key = '/home/dle10kg/tethysapp-ecocat/tethysapp/ecocat/private/ecocat-dev-1ce9d3b90dc8.json'
+service_account = ''
+private_key = ''
